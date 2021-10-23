97b5f3e9acac93cc9261eae5eebcbd0c4a23fe08
Matthieu Muller <mullerm1002@gmail.com>
2021-10-23 15:24:59 +0200

    reservations controller created

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,8 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @offers = Offer.all
+    @offers = Offer.all.select { |offer| offer.reservation.nil? || "en attente de validation" }
   end
 
   def new
@@ -9,27 +11,24 @@ class OffersController < ApplicationController
 
   def create
     @offer = Offer.new(offer_params)
+    @offer.user = current_user
     @offer.save!
     redirect_to offer_path(@offer)
   end
 
   def edit
-    @offer = Offer.find(params[:id])
   end
 
   def show
-    @offer = Offer.find(params[:id])
   end
 
   def update
-    @offer = Offer.find(params[:id])
     @offer.update(offer_params)
 
     redirect_to offer_path(@offer)
   end
 
   def destroy
-    @offer = Offer.find(params[:id])
     @offer.destroy
 
     redirect_to offers_path
@@ -39,5 +38,9 @@ class OffersController < ApplicationController
 
   def offer_params
     params.require(:offer).permit(:title, :address, :date, :description, :image, :meal)
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:id])
   end
 end
