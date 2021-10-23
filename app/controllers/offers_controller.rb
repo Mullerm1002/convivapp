30a1a28fd01749c5ca933302d0056fc73f589d1f
Matthieu Muller <mullerm1002@gmail.com>
2021-10-23 12:20:16 +0200

    crud offers created

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,21 +4,40 @@ class OffersController < ApplicationController
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    @offer.save!
+    redirect_to offer_path(@offer)
   end
 
   def edit
+    @offer = Offer.find(params[:id])
   end
 
   def show
     @offer = Offer.find(params[:id])
   end
 
-  def udpate
+  def update
+    @offer = Offer.find(params[:id])
+    @offer.update(offer_params)
+
+    redirect_to offer_path(@offer)
   end
 
   def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+
+    redirect_to offers_path
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :address, :date, :description, :image, :meal)
   end
 end
