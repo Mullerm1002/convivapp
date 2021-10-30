a63e82c4707a14b727b5ef01ae8eb6038582b89d
Matthieu Muller <mullerm1002@gmail.com>
2021-10-30 17:56:19 +0200

    dashboard without front

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  enum status: {
+    "pending" => 0,
+    "accepted" => 1,
+    "denied" => 2
+  }
+
+  def start_time
+    offer.date
+  end
 end
