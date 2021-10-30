1c00c49eb00ce64399621aa2aa8d4a33224bc63e
alexandre lecaplain <alexandre.lecaplain@gmail.com>
2021-10-30 17:45:56 +0200

    first draft for google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
   has_many :offers
   has_many :reservations
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+    def self.from_omniauth(access_token)
+      binding.pry
+      data = access_token.info
+      user = User.where(email: data['email']).first
+      puts "#{data}"
+      # Uncomment the section below if you want users to be created if they don't exist
+      # unless user
+      #     user = User.create(name: data['name'],
+      #        email: data['email'],
+      #        password: Devise.friendly_token[0,20]
+      #     )
+      # end
+      user
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  get 'auth/:provider/callback', to: 'session#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers do
     resources :reservations, only: [:new, :create]
